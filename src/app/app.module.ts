a3f04c6edb41e33d086441fa2e1ef60e70c87d80
VitorHenrique-ViniciusFaoro <VitorHenrique-ViniciusFaoro@test.com>
2023-03-14 10:44:27 -0300

    Criacao do componente personagem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { PersonagemComponent } from './personagem/personagem.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PersonagemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/personagem/personagem.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PersonagemComponent } from './personagem.component';
+
+describe('PersonagemComponent', () => {
+  let component: PersonagemComponent;
+  let fixture: ComponentFixture<PersonagemComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PersonagemComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PersonagemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
